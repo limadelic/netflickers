e6575d7a42ec6fd1e465a2f29a3b1fbe69cd0333
maykels <maykel.suarez@ukg.com>
2021-06-24 17:42:24 -0400

    spiders spike

---
+++ esl_blog/mix.exs
@@ -0,0 +1,29 @@
+defmodule EslBlog.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :esl_blog,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {EslBlog.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:crawly, "~> 0.13.0"},
+      {:floki, "~> 0.26.0"}
+    ]
+  end
+end
